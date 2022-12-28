43c75a9c4744bb04213e486c8a3e6d55b2098da3
Vasyl Petashkevych <petrashkevych.v@gmail.com>
2022-12-29 01:25:36 +0200

    Update configuration

---
+++ README.md
@@ -0,0 +1,6 @@
+# WordPress Docker configuration
+
+## How to use
+Add your user  to http group ``sudo usermod -aG http $USER``
+
+You need on first time run ``start.sh`` for next times just ``docker compose up -d`` or ``docker compose up``
--- server/scripts/install-wordpress.sh
+++ server/scripts/install-wordpress.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 PATH_TO_THEMES="/var/www/html/wp-content/themes"
+
 RED='\033[0;31m'
 NC='\033[0m'
 GREEN='\033[0;32m'
@@ -17,41 +18,41 @@ if [ -d ${PATH_TO_THEMES} ]; then
 else
   echo -e "${RED}Start install WordPress${NC}"
   wp core download --path="/var/www/html/" --allow-root
-  wp core install --path="/var/www/html/" --url=wisehunter.localhost --title="Your Blog Title" --admin_name=admin --admin_password=admin --admin_email=you@example.com --allow-root
+  wp core install --path="/var/www/html/" --url="https://${SERVER_NAME}" --title="Your Blog Title" --admin_name=admin --admin_password=admin --admin_email=you@example.com --allow-root
 
-  echo "Deactivate plugin akismet"
+  echo -e "${RED}Deactivate plugin akismet${NC}"
   wp plugin deactivate akismet --path="/var/www/html/"  --allow-root
 
-  echo "Deactivate plugin hello"
+  echo -e "${RED}Deactivate plugin hello${NC}"
   wp plugin deactivate hello --path="/var/www/html/"  --allow-root
 
-  echo "Delete plugin akismet"
+  echo -e "${RED}Delete plugin akismet${NC}"
   wp plugin delete akismet --path="/var/www/html/"  --allow-root
 
-  echo "Delete plugin hello"
+  echo -e "${RED}Delete plugin hello${NC}"
   wp plugin delete hello --path="/var/www/html/"  --allow-root
 
-  echo "Install plugin query-monitor"
+  echo -e "${RED}Install plugin query-monitor${NC}"
   wp plugin install query-monitor --activate --path="/var/www/html/"  --allow-root
 
-  echo "Install plugin wordpress-seo"
+  echo -e "${RED}Install plugin wordpress-seo${NC}"
   wp plugin install wordpress-seo --activate --path="/var/www/html/"  --allow-root
 
-  echo "Install plugin wp-mail-smtp"
+  echo -e "${RED}Install plugin wp-mail-smtp${NC}"
   wp plugin install wp-mail-smtp --activate --path="/var/www/html/"  --allow-root
 
-  echo "Change Permalink structure"
+
+  echo -e "${RED}Change Permalink structure${NC}"
   wp rewrite structure '/%postname%/' --path="/var/www/html/"  --allow-root
 
-  echo "Delete all pages"
+  echo -e "${RED}Delete all pages${NC}"
   wp post delete $(wp post list --post_type='page' --format=ids --path="/var/www/html/"  --allow-root ) --path="/var/www/html/"  --allow-root --force
 
-  echo "Delete all posts"
+  echo -e "${RED}Delete all posts${NC}"
   wp post delete $(wp post list --post_type='post' --format=ids --path="/var/www/html/"  --allow-root ) --path="/var/www/html/"  --allow-root --force
 
-  echo "Create Home Page"
+  echo -e "${RED}Create Home Page${NC}"
   wp post create --post_type=page --post_title='Home'  --post_status=publish --path="/var/www/html/"  --allow-root
 fi
 
-
-chown -R www-data:www-data /var/www/html
+chown -R www-data:www-data /var/www/html/
